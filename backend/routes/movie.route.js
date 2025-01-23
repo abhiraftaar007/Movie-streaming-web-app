4a2bfc3a5788a219ceafdb6ba829903ca58b7bdb
abhiraftaar007 <singhabhishekas786@gmail.com>
2025-01-23 18:43:11 +0530

    TMDB API, Movie routes for trending details and Trailer, Movie controller implementation, tmdb services for fetching data

---
+++ backend/routes/movie.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getMovieDetails, getMovieTrailers, getTrendingMovie } from "../controllers/movie.controller.js";
+
+const router = express.Router();
+
+router.get("/trending", getTrendingMovie);
+router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 // const express = require('express');
 import express from "express";
+
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 // import dotenv from "dotenv"
@@ -15,6 +18,7 @@ app.use(express.json()); // will allow us to parse req.body
 // console.log("MONGO_URI: ", process.env.MONGO_URI);
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
@@ -22,3 +26,4 @@ app.listen(PORT, () => {
 });
 
 // pqpHZXGIGJE8pX1p
+
