e9975234981fa119fe0865be24dd9b310db5910c
abhiraftaar007 <singhabhishekas786@gmail.com>
2025-02-01 16:03:45 +0530

    Protecting Routes by unauthorized access, also called middleware

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ import tvRoutes from "./routes/tv.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+import cookieParser from "cookie-parser";
+
 // import dotenv from "dotenv"
 
 // dotenv.config();
@@ -15,12 +18,13 @@ const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // will allow us to parse req.body
+app.use(cookieParser());
 
 // console.log("MONGO_URI: ", process.env.MONGO_URI);
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
