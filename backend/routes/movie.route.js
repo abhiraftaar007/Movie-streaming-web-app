97ceec6a406d61f3a673f74b2ee70c2242302e69
abhiraftaar007 <singhabhishekas786@gmail.com>
2025-02-01 15:26:14 +0530

    Movie route for similar and category, movie controller updated

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getMovieDetails, getMovieTrailers, getTrendingMovie } from "../controllers/movie.controller.js";
+import { getMovieDetails, getMoviesByCategory, getMovieTrailers, getSimilarMovies, getTrendingMovie } from "../controllers/movie.controller.js";
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
 router.get("/:id/trailers", getMovieTrailers);
 router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovies);
+router.get("/:category", getMoviesByCategory);
 
 export default router;
