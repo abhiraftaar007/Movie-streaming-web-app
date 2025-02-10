d3549b12fcc055cf5bad6c953764fdecd094321b
abhiraftaar007 <singhabhishekas786@gmail.com>
2025-02-10 14:42:33 +0530

    Login, Logout, Signup, AuthCheck, AuthUser in store for backend implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,47 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authUser";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 function App() {
+  const { user, isCheckingAuth, authCheck } = useAuthStore();
+  console.log("auth user is here: ", user);
+
+  useEffect(() => {
+    authCheck();
+  }, [authCheck]);
+
+  if (isCheckingAuth) {
+    return (
+      <div className="h-screen">
+        <div className="flex justify-center items-center bg-black h-full">
+          <Loader className="animate-spin text-red-600 size-10" />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route
+          path="/login"
+          element={!user ? <LoginPage /> : <Navigate to={"/"} />}
+        ></Route>
+        <Route
+          path="/signup"
+          element={!user ? <SignUpPage /> : <Navigate to={"/"} />}
+        ></Route>
       </Routes>
 
-      <Footer/>
+      <Footer />
+      <Toaster />
     </>
   );
 }
--- frontend/src/pages/Home/HomeScreen.jsx
+++ frontend/src/pages/Home/HomeScreen.jsx
@@ -1,8 +1,12 @@
+import { useAuthStore } from "../../store/authUser"
 
 const HomeScreen = () => {
+  const {logout} = useAuthStore();
   return (
     <div>
     Homescreen
+
+    <button onClick={logout}>logout</button>
     </div>
   )
 }
